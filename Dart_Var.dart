ff31e95cf0ee7770d2bcbd90e97809e3f62a1700
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2024-07-09 09:03:23 +0300

    Dart_Variables

---
+++ Dart_Var.dart
@@ -0,0 +1,34 @@
+void main() {
+  //string
+  String name = "abdo";
+  print(name);
+  //integer
+  int num = 23;
+  print(num);
+  //Double
+  double nm2 = 22.5;
+  print(nm2);
+  //Boolean
+  bool test = false;
+  print(test);
+  //Dynamic Var :
+  dynamic gee = 'gee 24';
+  print(gee);
+  gee = 55;
+  print(gee);
+  //Final And Const Keyword in Dart These keywords are used to define constant variable in Dart i.e. once a variable is defined using these keyword then its value can’t be changed in the entire code. These keyword can be used with or without data type name.
+  // 1-Final
+  final REBACK = 12;
+  print(REBACK);
+
+  final String KKK = "YYYY";
+  print(KKK);
+
+  // 2-Const
+  const ahmed = "gg";
+  print(ahmed);
+  // 3-Null safety
+  int? a;
+  a = null;
+  print(a);
+}
